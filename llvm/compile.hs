343a12dec84c9bedb78b2f108cf43f7409e16852
Brandon Cook <phylake@gmail.com>
2013-04-08 09:34:20 -0700

    multinames

--- llvm/compile.hs
+++ llvm/compile.hs
@@ -41,21 +41,23 @@ alterMethodBody = undefined
 main :: IO ()
 main = do
   abc@(Abc ints uints doubles strings nsInfo nsSet multinames methodSigs metadata instances classes scripts methodBodies) <- E.run_ (EB.enumFile "abc/Test.abc" E.$$ parseAbc)
+  let (_, _, _, _, mres) = getResolutionMethods abc
   llvm_ints <- mapM (\(i, a) -> return $ "@.int_" ++ show i ++ " = constant i32 " ++ show a) $ zip (map fromIntegral [0..length ints]) ints
   llvm_uints <- mapM (\(i, a) -> return $ "@.uint_" ++ show i ++ " = constant i32 " ++ show a) $ zip (map fromIntegral [0..length uints]) uints
   llvm_doubles <- mapM (\(i, a) -> return $ "@.double_" ++ show i ++ " = constant double " ++ show a) $ zip (map fromIntegral [0..length doubles]) doubles
   llvm_strings <- mapM (\(i, a) -> return $ "@.string_" ++ show i ++ " = constant [" ++ show (length a + 1) ++ " x i8] c\"" ++ a ++ "\\00\"") $ zip (map fromIntegral [0..length strings]) strings
+  --mapM (\(i, a) -> put_nsInfo cp idx a) $ zip (map fromIntegral [0..length nsInfo]) nsInfo
+  --mapM (\(i, a) -> put_nsSet cp idx a) $ zip (map fromIntegral [0..length nsSet]) nsSet
+  llvm_multinames <- mapM (\(i, a) -> return $ "@.mn_" ++ show i ++ " = constant [" ++ show (length (mres i) + 1) ++ " x i8] c\"" ++ mres i ++ "\\00\"") $ zip (map fromIntegral [0..length multinames]) multinames
   writeFile "llvm/test.ll" $
        unlines llvm_ints
     ++ unlines llvm_uints
     ++ unlines llvm_doubles
     ++ unlines llvm_strings
+    ++ unlines llvm_multinames
     -- ++ unlines (map show $ xform_methodBodies abc)
     ++ "\n"
 
-  --mapM (\(i, a) -> put_nsInfo cp idx a) $ zip (map fromIntegral [0..length nsInfo]) nsInfo
-  --mapM (\(i, a) -> put_nsSet cp idx a) $ zip (map fromIntegral [0..length nsSet]) nsSet
-  --mapM (\(i, a) -> put_multiname cp idx a) $ zip (map fromIntegral [0..length multinames]) multinames
   --mapM (\(i, a) -> put_methodSig cp idx a) $ zip (map fromIntegral [0..length methodSigs]) methodSigs
   --mapM (\(i, a) -> put_metadata cp idx a) $ zip (map fromIntegral [0..length metadata]) metadata
   --mapM (\(i, a) -> put_instance cp idx a) $ zip (map fromIntegral [0..length instances]) instances
@@ -63,7 +65,41 @@ main = do
   --mapM (\(i, a) -> put_script cp idx a) $ zip (map fromIntegral [0..length scripts]) scripts
   --mapM (\(i, a) -> put_methodBody cp idx a) $ zip (map mbMethod methodBodies) methodBodiesNew
   return ()
-  
+
+getResolutionMethods :: Abc -> (
+                                 U30 -> S32    -- int
+                               , U30 -> U30    -- uint
+                               , U30 -> Double -- double
+                               , U30 -> String -- string
+                               , U30 -> String -- multiname
+                               )
+getResolutionMethods (Abc ints uints doubles strings nsInfo nsSet multinames methodSigs metadata instances classes scripts methodBodies) =
+    (
+      int_res
+    , uint_res
+    , double_res
+    , string_res
+    , multiname_res
+    )
+  where
+    int_res :: U30 -> S32
+    int_res i = ints !! fromIntegral i
+
+    uint_res :: U30 -> U30
+    uint_res i = uints !! fromIntegral i
+
+    double_res :: U30 -> Double
+    double_res i = doubles !! fromIntegral i
+
+    string_res :: U30 -> String
+    string_res i = strings !! fromIntegral i
+
+    multiname_res :: U30 -> String
+    multiname_res i = multiname_impl string_res nsinfo_res $ multinames !! fromIntegral i
+
+    nsinfo_res :: U30 -> String
+    nsinfo_res i = nsinfo_impl string_res$ nsInfo !! fromIntegral i
+
 xform_methodBodies :: Abc -> M.Map Int [LLVMOp]
 xform_methodBodies (Abc ints uints doubles strings nsInfo nsSet multinames methodSigs metadata instances classes scripts methodBodies) =
   --map extractOpCode methodBodies
@@ -92,73 +128,52 @@ xform_methodBodies (Abc ints uints doubles strings nsInfo nsSet multinames metho
     extractOpCode :: MethodBody -> [OpCode]
     extractOpCode (MethodBody _ _ _ _ _ code _ _) = code
 
-    int_res :: U30 -> S32
-    int_res i = ints !! fromIntegral i
 
-    uint_res :: U30 -> U30
-    uint_res i = uints !! fromIntegral i
-
-    double_res :: U30 -> Double
-    double_res i = doubles !! fromIntegral i
-
-    multiname_res :: U30 -> Maybe B.ByteString
-    multiname_res i = maybeMultiname string_res nsinfo_res $ multinames !! fromIntegral i
-
-    string_res :: U30 -> B.ByteString
-    string_res i = BC.pack$ strings !! fromIntegral i
-
-    nsinfo_res :: U30 -> B.ByteString
-    nsinfo_res i = nsinfo_raw string_res$ nsInfo !! fromIntegral i
-
-maybeMultiname :: (U30 -> B.ByteString) -- string resolution
-               -> (Abc.U30 -> B.ByteString) -- nsinfo resolution
-               -> Abc.Multiname
-               -> Maybe B.ByteString
-maybeMultiname string_res nsinfo_res (Abc.Multiname_QName ns str)
-  | B.null nsinfo = Just string
-  | otherwise = Just$ B.append nsinfo$ B.append colons string
+multiname_impl :: (U30 -> String) -- string resolution
+               -> (U30 -> String) -- nsinfo resolution
+               -> Multiname
+               -> String
+multiname_impl string_res nsinfo_res (Multiname_QName ns str)
+  | nsinfo == "" = string
+  | otherwise = nsinfo ++ "::" ++ string
   where
     nsinfo = nsinfo_res ns
     string = string_res str
-    colons = BC.pack "::"
-maybeMultiname string_res nsinfo_res (Abc.Multiname_QNameA ns str)
-  | B.null nsinfo = Just string
-  | otherwise = Just$ B.append nsinfo$ B.append colons string
+multiname_impl string_res nsinfo_res (Multiname_QNameA ns str)
+  | nsinfo == "" = string
+  | otherwise = nsinfo ++ "::" ++ string
   where
     nsinfo = nsinfo_res ns
     string = string_res str
-    colons = BC.pack "::"
-maybeMultiname string_res nsinfo_res (Abc.Multiname_Multiname str ns)
-  | B.null nsinfo = Just string
-  | otherwise = Just$ B.append nsinfo$ B.append colons string
+multiname_impl string_res nsinfo_res (Multiname_Multiname str ns)
+  | nsinfo == "" = string
+  | otherwise = nsinfo ++ "::" ++ string
   where
     nsinfo = nsinfo_res ns
     string = string_res str
-    colons = BC.pack "::"
-maybeMultiname string_res nsinfo_res (Abc.Multiname_MultinameA str ns)
-  | B.null nsinfo = Just string
-  | otherwise = Just$ B.append nsinfo$ B.append colons string
+multiname_impl string_res nsinfo_res (Multiname_MultinameA str ns)
+  | nsinfo == "" = string
+  | otherwise = nsinfo ++ "::" ++ string
   where
     nsinfo = nsinfo_res ns
     string = string_res str
-    colons = BC.pack "::"
-maybeMultiname _ _ (Abc.Multiname_RTQName a) = Nothing
-maybeMultiname _ _ (Abc.Multiname_RTQNameA a) = Nothing
-maybeMultiname _ _ (Abc.Multiname_MultinameL a) = Nothing
-maybeMultiname _ _ (Abc.Multiname_MultinameLA a) = Nothing
-maybeMultiname _ _ Abc.Multiname_Any = Nothing
+multiname_impl _ _ (Multiname_RTQName a) = "Multiname_RTQName"
+multiname_impl _ _ (Multiname_RTQNameA a) = "Multiname_RTQNameA"
+multiname_impl _ _ (Multiname_MultinameL a) = "Multiname_MultinameL"
+multiname_impl _ _ (Multiname_MultinameLA a) = "Multiname_MultinameLA"
+multiname_impl _ _ Multiname_Any = "*"
 
-nsinfo_raw :: (Abc.U30 -> B.ByteString) -- string resolution
-           -> Abc.NSInfo
-           -> B.ByteString
-nsinfo_raw string_res (Abc.NSInfo_Namespace a)          = string_res a
-nsinfo_raw string_res (Abc.NSInfo_PackageNamespace a)   = string_res a
-nsinfo_raw string_res (Abc.NSInfo_PackageInternalNs a)  = string_res a
-nsinfo_raw string_res (Abc.NSInfo_ProtectedNamespace a) = string_res a
-nsinfo_raw string_res (Abc.NSInfo_ExplicitNamespace a)  = string_res a
-nsinfo_raw string_res (Abc.NSInfo_StaticProtectedNs a)  = string_res a
-nsinfo_raw string_res (Abc.NSInfo_PrivateNs a)          = string_res a
-nsinfo_raw string_res Abc.NSInfo_Any = BC.pack "*"
+nsinfo_impl :: (U30 -> String) -- string resolution
+            -> NSInfo
+            -> String
+nsinfo_impl string_res (NSInfo_Namespace a)          = string_res a
+nsinfo_impl string_res (NSInfo_PackageNamespace a)   = string_res a
+nsinfo_impl string_res (NSInfo_PackageInternalNs a)  = string_res a
+nsinfo_impl string_res (NSInfo_ProtectedNamespace a) = string_res a
+nsinfo_impl string_res (NSInfo_ExplicitNamespace a)  = string_res a
+nsinfo_impl string_res (NSInfo_StaticProtectedNs a)  = string_res a
+nsinfo_impl string_res (NSInfo_PrivateNs a)          = string_res a
+nsinfo_impl string_res NSInfo_Any                    = "*"
 
 {-xform_methodBodies2 :: (Abc.IntIdx -> Abc.S32)                  -- int resolution
                     -> (Abc.UintIdx -> Abc.U32)                 -- uint resolution
